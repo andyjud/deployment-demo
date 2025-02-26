364925f777c11d3c314b8d7bf5304962234b7af5
andyjud <andreas.jud@gmail.com>
2025-02-26 10:35:09 +0100

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -37,9 +37,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web-ge1i4ftjsl6p.up-de-fra1-k8s-1.apps.run-on-seenode.com/']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://web-ge1i4ftjsl6p.up-de-fra1-k8s-1.apps.run-on-seenode.com/' ]
 
 
 # Application definition
