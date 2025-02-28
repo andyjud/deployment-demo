279cb4648d3a5a91160f0588054686382d911782
andyjud <andreas.jud@gmail.com>
2025-02-28 17:54:34 +0100

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -21,17 +21,11 @@ ENVIRONMENT = env('ENVIRONMENT', default="production")
 
 POSTGRES_LOCALLY = False
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'development':
     DEBUG = True 
 else:
